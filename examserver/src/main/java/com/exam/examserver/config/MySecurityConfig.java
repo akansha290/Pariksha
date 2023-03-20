9010faaf593ced23100c6d418c47c883d642e029
akansha290 <akanshakumari290@gmail.com>
2023-03-20 12:16:08 +0530

    Added comments to understand Spring Security.

--- examserver/src/main/java/com/exam/examserver/config/MySecurityConfig.java
+++ examserver/src/main/java/com/exam/examserver/config/MySecurityConfig.java
@@ -37,7 +37,19 @@ public class MySecurityConfig {
         return NoOpPasswordEncoder.getInstance();
     }
 
-
+    //This is a method in Java that returns an instance of the AuthenticationManager interface. The AuthenticationManager interface is a part of Spring Security framework and is responsible for authenticating users based on their credentials, such as username and password.
+    //
+    //The method takes an instance of the AuthenticationConfiguration class as a parameter. The AuthenticationConfiguration class is used to configure the authentication process, such as setting up user details service, authentication provider, and other authentication-related settings.
+    //
+    //In the provided code, the method simply returns the AuthenticationManager instance obtained from the AuthenticationConfiguration object by calling the getAuthenticationManager() method. This means that the authentication manager used in the application is obtained from the AuthenticationConfiguration object.
+    //
+    //The AuthenticationManager interface has a single method, authenticate(), which is responsible for authenticating users based on their credentials. When a user attempts to log in, their credentials are passed to the authenticate() method, which uses the configured UserDetailsService to retrieve the user's details, and then uses the configured PasswordEncoder to compare the provided password with the stored hash.
+    //
+    //If the provided credentials match the stored credentials, the authenticate() method returns an instance of the Authentication interface, which represents a successful authentication. If the provided credentials do not match the stored credentials, the authenticate() method throws an instance of the AuthenticationException interface, which represents a failed authentication.
+    //
+    //The AuthenticationManager interface is typically used in conjunction with other Spring Security components, such as AuthenticationProvider and UserDetailsService, to provide a complete authentication and authorization solution for a Spring Security-enabled application.
+    //
+    //In summary, the authenticationManager() method is responsible for returning an instance of the AuthenticationManager interface that is used to authenticate users based on their credentials in a Spring Security-enabled application. This method obtains the AuthenticationManager instance from the AuthenticationConfiguration object.ā
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
@@ -63,6 +75,17 @@ public class MySecurityConfig {
         return http.build();
     }
 
+    //This is a method in Java that returns an instance of the WebMvcConfigurer interface. The WebMvcConfigurer interface is a part of the Spring Web MVC framework and provides configuration options for web application context, such as configuring CORS (Cross-Origin Resource Sharing) settings.
+    //
+    //The method overrides the addCorsMappings() method of the WebMvcConfigurer interface to configure the CORS settings for the application. The addCorsMappings() method takes a CorsRegistry object as a parameter, which is used to configure the CORS settings.
+    //
+    //In the provided code, the addCorsMappings() method configures the CORS settings to allow requests from any origin and with any HTTP method by calling the addMapping() method on the CorsRegistry object with a mapping of "/**" and the allowedMethods() method with "*" as its parameter.
+    //
+    //The "/**" mapping allows the CORS configuration to be applied to all endpoints in the application, while the "*" parameter for allowedMethods() indicates that any HTTP method is allowed.
+    //
+    //CORS is a security feature implemented in web browsers that restricts web pages from making requests to a different domain than the one that served the original page. CORS allows the web page to specify which other domains are allowed to access its resources. By default, web browsers enforce CORS to prevent malicious websites from accessing sensitive data from other websites.
+    //
+    //In summary, the corsConfigurer() method is responsible for returning an instance of the WebMvcConfigurer interface that is used to configure the CORS settings for the application. The addCorsMappings() method is overridden to configure the CORS settings to allow requests from any origin and with any HTTP method. This method is typically used in a Spring Web MVC-enabled application to configure CORS settings for the endpoints.
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
